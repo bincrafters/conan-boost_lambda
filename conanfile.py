bda02187f7145416e31216261eaf3a727665645d
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:05 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,23 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostLambdaConan(base.BoostBaseConan):
     name = "boost_lambda"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_lambda"
-    lib_short_names = ["lambda"]
-    header_only_libs = ["lambda"]
-    b2_requires = [
-        "boost_bind",
-        "boost_config",
-        "boost_core",
-        "boost_detail",
-        "boost_iterator",
-        "boost_mpl",
-        "boost_preprocessor",
-        "boost_tuple",
-        "boost_type_traits",
-        "boost_utility"
-    ]
+    version = "1.70.0"
